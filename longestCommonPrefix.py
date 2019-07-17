baa9c94f463b25540c510a5da2579a9802c3c006
EugenenZhou <735159373@qq.com>
2019-07-18 00:12:19 +0800

    update

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -2,10 +2,21 @@
 #
 # 如果不存在公共前缀，返回空字符串 ""。
 ######################################################################
-def longestCommonPrefix(ss):
-
-    return ''
+def longestCommonPrefix(strs):
+    if not strs or '' in strs: return ''
+    temp = strs[0]
+    for i in range(1, len(strs)):
+        for j in range(len(temp)):
+            if temp[j] != strs[i][j] and j == 0:
+                return ''
+            if temp[j] != strs[i][j]:
+                temp = temp[0:j]
+                break
+            if j == len(strs[i])-1:
+                temp = temp[0:j+1]
+                break
+    return temp
 ######################################################################
-string = ["dog","racecar","car"]
+string = ['','','a']
 result = longestCommonPrefix(string)
 print(result)
